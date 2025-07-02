7843f5b7164a22758764fac025dce90e5149391a
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-07-02 17:58:06 +0530

    finalizing styling

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,3 +1,4 @@
+import './AnimalShow.css'
 import {useState} from 'react';
 import bird from './svg/bird.svg';
 import cat from './svg/cat.svg';
@@ -24,13 +25,9 @@ function AnimalShow({type}){
 };
 
     return(
-        <div onClick={handleClick}>
-            <img alt='animals' src={svgMap[type]}/>
-            <img 
-            alt="heart" 
-            src={heart}
-            style={{width:10+10*click + 'px'}}
-            />
+        <div className="animal-show" onClick={handleClick}>
+            <img className="animals" alt='animals' src={svgMap[type]}/>
+            <img className="heart" alt="heart" src={heart} style={{width:10+10*click + 'px'}}/>
         </div>
     )
 }
--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import {useState} from 'react'
 import AnimalShow from './AnimalShow'
 
@@ -23,9 +24,9 @@ function App(){
         return <AnimalShow type={animals}  key={index} />
     })
     return(
-        <div>
+        <div className="app">
             <button onClick={handleClick}>Add Animals</button>
-            <div>{renderedAnimals} </div>
+            <div className="animal-list">{renderedAnimals} </div>
         </div>
 
     );
